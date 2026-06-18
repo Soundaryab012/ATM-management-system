834804bebb02495844ad29162a6cfa8161d62169
Soundaryab012 <soundaryab012@gmail.com>
2026-06-18 10:21:46 +0530

    five commit

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,26 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = require("./config/db");
-
 connectDB();
 
 const app = express();
 
-app.use(cors("https://atm-management-system-eoqu.vercel.app"));
+app.use(cors({
+  origin: true,
+
+}));
+
 app.use(express.json());
 
 app.use("/api/atm", require("./routes/atmRoutes"));
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
-
   res.status(500).json({
     message: err.message,
   });
 });
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server Running");
 });
